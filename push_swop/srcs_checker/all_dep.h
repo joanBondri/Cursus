f0f79d993b753f682f4781782355fbb8eab66958
Joan BONDRI <jbondri@e1r10p2.42.fr>
2021-09-18 18:43:57 +0200

    AAAAAAAAAAAAAAAAh

---
+++ push_swop/srcs_checker/all_dep.h
@@ -0,0 +1,9 @@
+#ifndef ALL_DEP_H
+# define ALL_DEP_H
+# include <stdlib.h>
+# include <limits.h>
+# include <stdio.h>
+# include "../libft/libft.h"
+# include "utils.h"
+# include "checker.h"
+#endif
---
+++ push_swop/srcs_checker/checker.h
@@ -0,0 +1,11 @@
+#ifndef CHECKER_H
+# define CHECKER_H
+# include "printf.h"
+# include <stdbool.h>
+
+typedef struct		s_flags
+{
+	bool	view;
+}					t_flags;
+
+#endif
