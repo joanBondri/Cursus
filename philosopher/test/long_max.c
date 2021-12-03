75fbe9acabff9ddb19dcc6a081b5a8ed7e5cfc9b
Joan BONDRI <jbondri@e2r4p4.clusters.42paris.fr>
2021-12-03 14:56:42 +0100

    on enleve les dechets

--- philosopher/test/long_max.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-#include <limits.h>
-
-int	main(void)
-{
-	printf("long max : %ld, long min : %ld\n", LONG_MAX, LONG_MIN);
-	printf("long long max : %lld, long long mi n : %lld\n", LLONG_MAX, LLONG_MIN);
-	return (0);
-}
